90f52055e5e831cc5856335621d9e9a24ab48c0c
VynJs <joshbel31@gmail.com>
2026-06-15 13:45:55 -0600

    Modelos

---
+++ models/libro.py
@@ -0,0 +1,20 @@
+class Libro:
+    
+    #Constructor
+    def __init__(self, id, titulo, autor, isbn, disponible):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.isbn = isbn
+        self.disponible = disponible
+    
+    def prestar(self):
+        self.disponible = False
+
+    def prestar(self):
+        self.disponible = True
+        
+
+    def mostrar_info(self):
+        disponible = "Disponible" if self.disponible else "No disponible"
+        return f"ID:  {self.id}, Ttiulo: {self.titulo}, Autor: {self.autor.nombre}, ISBN: {self.isbn}, Estado: {disponible}"
